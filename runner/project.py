877398ca5079f07892004e4bb6634b99eb200ff6
semwai <e14s@mail.ru>
2022-10-26 00:56:34 +0300

    added project & commands classes

--- runner/project.py
+++ runner/project.py
@@ -1,14 +1,44 @@
-from runner.container import Container, Command
+from runner.container import Container
+from abc import ABC
+from dataclasses import dataclass
 
 
-class Step:
+@dataclass
+class Step(ABC):
+    """Абстрактная команда"""
+    pass
 
-    def __init__(self, command: Command):
-        self.command = command
+
+@dataclass
+class AddFile(Step):
+    """Добавление файла"""
+    name: str
+    data: str | bytes
+
+
+@dataclass
+class RunCommand(Step):
+    """Запуск консольной команды внутри контейнера"""
+    command: str
+    # читать из stdout/stderr
+    read: bool
+    # писать в stdin
+    write: bool
 
 
 class Project:
-
-    def __init__(self, steps: Step, container: Container):
+    """Проект руководит командами и обеспечивает их правильное выполнение"""
+    def __init__(self, container: Container, *steps):
         self.steps = steps
+        self.container = container
 
+
+if __name__ == '__main__':
+    text = """
+    print(123 + 321)
+    """
+    p = Project(
+        Container('python:3.10-alpine'),
+        AddFile('app.py', text),
+        RunCommand('python app.py', read=True, write=True)
+    )
