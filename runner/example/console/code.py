641b3063d189dbfc0dc9ddb994be52d0cce17e81
semwai <e14s@mail.ru>
2022-10-25 13:21:39 +0300

    update console example

---
+++ runner/example/console/code.py
@@ -0,0 +1,9 @@
+import time
+print("hello")
+a = 2
+b = int(input("b = "))
+print(f"({a=})+({b=})")
+time.sleep(1)
+print(f"({a=})+({b=})={a+b}")
+c = int(input("10/c, c ="))
+print(10/c)
--- runner/example/console/python_runner.py
+++ runner/example/console/python_runner.py
@@ -1,25 +1,11 @@
 from runner.container import Container
 
-code = """
-import time
-print("hello")
-a = 2
-b = int(input("b = "))
-print(f"({a=})+({b=})")
-time.sleep(1)
-print(f"({a=})+({b=})={a+b}")
-c = int(input("10/c, c ="))
-print(10/c)
-"""
-
 
 def main():
     container = Container('python:3.10-alpine')
-    container.add_file('app.py', code)
-    # Выполняем команду и получаем сокет для ввода-вывода
+    container.add_file('app.py', open('example/console/code.py').read())
     command = container.command('python app.py')
-    # wait for run
-    while (c:=input('command:')) != 'exit':
+    while (c := input('command:')) != 'exit':
         match c:
             case 'read':
                 print(command.read())
