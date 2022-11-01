e87fdab3bdf1998436f7c7daccc6a6f0d10cc032
semwai <e14s@mail.ru>
2022-11-01 16:15:32 +0300

    move example with console thread

--- runner/project.py
+++ runner/project.py
@@ -1,7 +1,6 @@
 import time
-
 from runner.container import Container
-from runner.controller import Controller, ThreadConsoleController
+from runner.controller import Controller
 from runner.step import AddFile, RunCommand
 
 
@@ -58,50 +57,3 @@ class Project:
 
     def __del__(self):
         del self.container
-
-
-def main():
-    text = """
-package main
-import (
-    "fmt"
-    "time"
-)
-func f(from string) {
-    for i := 0; i < 2; i++ {
-        fmt.Println(from, ":", i)
-        time.Sleep(time.Second)
-    }
-}
-func main() {
-    var text string
-    fmt.Print("enter value:")
-    fmt.Scan(&text)
-    f(text)
-    go f("goroutine1")
-    go f("goroutine2")
-    time.Sleep(time.Second * 3)
-    fmt.Println("done")
-}
-        """
-    controller = ThreadConsoleController()
-    project = Project(
-        controller,
-        Container('golang:alpine'),
-        AddFile('main.go', text),  # Вместо text будет описание источника ввода файла
-        RunCommand('go build main.go', stdin=False, stdout=True),
-        RunCommand('ls -la', stdin=False, stdout=True),
-        RunCommand('./main', stdin=True, stdout=True)
-    )
-    controller.run()
-    try:
-        project.run()
-    except Exception as e:
-        print(e)
-    controller.stop()
-    del controller
-    del project
-
-
-if __name__ == '__main__':
-    main()
