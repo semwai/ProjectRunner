a4d91c6493ab87fb9db070b6150f4a5e59502ce1
semwai <e14s@mail.ru>
2022-11-02 00:01:50 +0300

    update front

--- runner/example/web_project/static/app.js
+++ runner/example/web_project/static/app.js
@@ -18,6 +18,7 @@ function start(event) {
         if (msg.ExitCode) {
             messages.innerHTML += `<span style="color:white;">${msg.ExitCode}</span>`
         }
+        messages.scrollTop += 1000
     };
 }
 
